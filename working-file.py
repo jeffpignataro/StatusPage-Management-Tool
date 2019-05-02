5c97ffeae10c84ec59cc79e268c91220dbe56adc
Jeff Pignataro <jeff.pignataro@sa.ucsb.edu>
2019-05-02 10:19:21 -0600

    Adding working-file to gitignore because it's not needed.

--- working-file.py
+++ working-file.py
@@ -1,4 +1,15 @@
 from controllers.component import *
 from helpers.jsonhelper import prettyprint
 
-prettyprint(getComponentsWithoutGroups())
+components = getComponents()
+for c in components:
+    if ((str(c["name"]).__contains__("Jira") or str(c["name"]).__contains__("Confluence")) and str(c["id"]) != "c0trd0wvb0f4"):
+        print(c["group_id"], end='|')
+        print(c["id"], end='|')
+        print(c["status"], end='|')
+        print(c["showcase"], end='|')
+        print(c["name"], end='|')
+        print(c["description"])
+        # if (not c["group_id"]):
+        #     print(updateComponent(c["id"], '', c["name"],
+        #                           c["status"], 'false', 'c0trd0wvb0f4'))
